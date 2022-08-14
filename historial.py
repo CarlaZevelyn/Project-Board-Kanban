fd1d57932d60ef4cca8e8a530d212d67fa7836ec
Marcos Peralta <69172841+marc0speralta@users.noreply.github.com>
2022-08-14 19:52:40 -0300

    Add files via upload

---
+++ historial.py
@@ -0,0 +1,6 @@
+historial = (2350, 5960, 23000, 1000, 8900)
+gasto = sum(list(historial))
+if gasto < 30000:
+    print ("Gasto Total: ", gasto)
+else:
+    print("Gastos por encima de lo presupuestado")
